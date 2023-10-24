53826339d231561909ecf81b76d9526327ea67e4
Svetajism <sv.zacharova@gmail.com>
2023-10-24 23:36:48 +0200

    Initial Commit

---
+++ SkyWalker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SkyWalker
+//
+//  Created by Svetlana Zakharova on 24/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SkyWalker/SkyWalkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyWalkerApp.swift
+//  SkyWalker
+//
+//  Created by Svetlana Zakharova on 24/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SkyWalkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
