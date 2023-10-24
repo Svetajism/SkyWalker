5d4551152c9be60b9d8791c6159998a52e632774
Svetajism <sv.zacharova@gmail.com>
2023-10-24 23:44:30 +0200

    SW-1: add project structure

--- SkyWalker/ContentView.swift
+++ SkyWalker/ContentView.swift
@@ -10,10 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            RoundedRectangle(cornerRadius: 20)
+                .foregroundColor(.orange)
+            
         }
         .padding()
     }
